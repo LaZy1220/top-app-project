3bd912cd2a3fb61372185c8097def6ea9f48a5f9
LaZy1220 <4elove4iska@gmail.com>
2023-03-23 16:13:02 +0300

    Add Tag

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
 import { Button } from "@/components/Button/Button";
 import { Htag } from "@/components/Htag/Htag";
+import { Paragraph } from "@/components/Paragraph/Paragraph";
+import { Tag } from "@/components/Tag/Tag";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -14,6 +16,15 @@ export default function Home(): JSX.Element {
       <Button appearance="ghost" arrow="down">
         Кнопка
       </Button>
+      <Paragraph size="s">Параграф S</Paragraph>
+      <Paragraph>Параграф M</Paragraph>
+      <Paragraph size="l">Параграф L</Paragraph>
+      <Tag size="s">Маленькиий</Tag>
+      <Tag color="red">Средний</Tag>
+      <Tag size="s" color="green" href="https://www.google.com/">
+        Маленькиий
+      </Tag>
+      <Tag color="primary">Средний</Tag>
     </>
   );
 }
