92e7ff995fdae85896a5c6bcc0608e9d8682794f
LaZy1220 <4elove4iska@gmail.com>
2023-03-30 13:21:31 +0300

    add hoc

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,16 +3,16 @@ import { Htag } from "@/components/Htag/Htag";
 import { Paragraph } from "@/components/Paragraph/Paragraph";
 import { Rating } from "@/components/Rating/Rating";
 import { Tag } from "@/components/Tag/Tag";
-import { Layout } from "@/layout/Layout";
+import { withLayout } from "@/layout/Layout";
 import { Inter } from "next/font/google";
 import { useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home(): JSX.Element {
+function Home(): JSX.Element {
   const [rating, setRating] = useState<number>(4);
   return (
-    <Layout>
+    <>
       <Htag tag="h1">ТЭГ</Htag>
       <Button appearance="primary" arrow="right">
         Кнопка
@@ -30,6 +30,7 @@ export default function Home(): JSX.Element {
       </Tag>
       <Tag color="primary">Средний</Tag>
       <Rating rating={rating} setRating={setRating} isEditable />
-    </Layout>
+    </>
   );
 }
+export default withLayout(Home);
